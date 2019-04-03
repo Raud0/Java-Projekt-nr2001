5b16e2807f325f7da641607e28965730af74ac0c
Raud0 <frederikraud1999@gmail.com>
2019-04-04 00:15:42 +0300

    paar lihvi

--- src/main/java/MAIN/Zen2001Programm.java
+++ src/main/java/MAIN/Zen2001Programm.java
@@ -63,23 +63,14 @@ public class Zen2001Programm {
 
     //tester, mis otsib sona ning leiab sealt ules naitekasutuse ja kasutusdomeeni
     public static String tester1(String sona) throws IOException {
-
         ResultsDTO results = Uurija.sonaleidja(sona);
-        //System.out.println(results);
         LexicalEntry lexicalEntries = results.getLexicalEntries().get((int)(Math.random()*results.getLexicalEntries().size()));
-        //System.out.println(lexicalEntries);
         Entry entries = lexicalEntries.getEntries().get((int)(Math.random()*lexicalEntries.getEntries().size()));
-        //System.out.println(entries);
         Sense senses = entries.getSenses().get((int)(Math.random()*entries.getSenses().size()));
-        //System.out.println(senses);
         Sense subsenses = senses.getSubsenses().get((int)(Math.random()*senses.getSubsenses().size()));
-        //System.out.println(subsenses);
         String domains = subsenses.getDomains().get((int)(Math.random()*subsenses.getDomains().size()));
-        //System.out.println(domains);
         Example example = subsenses.getExamples().get(0);
-        //System.out.println(examples);
         String text = example.getText();
-        //System.out.println(text);
         return domains + ": " + text;
     }
 
@@ -108,26 +99,22 @@ public class Zen2001Programm {
     public Zen2001Programm() {}
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        String testsone = "(*￣m￣)"; //vaikevaartus zen-meistri poolt, ausalt, voiks talle kaomoji pagasi anda | ヽ(￣ω￣(。。 )ゝ
-
-        //selle võiks jätta nulli peale, kui midagi, mis ei nõua päringute tegemist, testida, vähem raiskamist
+        String algtekst = "(*￣m￣)";
+        //testerid
+        // 0 väldib API küsitlemist
         int testmode = 2;
-
-        if (testmode == 1) //sona otsingu tester + korduvotsingu tester
-            testsone = tester1("set") + tester1("home") + tester1("set");
+        if (testmode == 1) //sona otsingu tester
+            algtekst = tester1("set");
         if (testmode == 2) //lause tester
-            testsone = tester2(" Why do they want Rathenau tonight? What did Caesar really whisper to his protégé as he fell? Et tu, Brute, the official lie, is about what you’d expect to get from them—it says exactly nothing. The moment of assassination is the moment when power and the ignorance of power come together, with Death as validator. When one speaks to the other then it is not to pass the time of day with et-tu-Brutes. What passes is a truth so terrible that history—at best a conspiracy, not always among gentlemen, to defraud—will never admit it. The truth will be repressed or in ages of particular elegance be disguised as something else. What will Rathenau, past the moment, years into a new otherside existence, have to say about the old dispensation? Probably nothing as incredible as what he might have said just as the shock flashed his mortal nerves, as the Angel swooped in…");
+            algtekst = tester2(" Why do they want Rathenau tonight? What did Caesar really whisper to his protégé as he fell? Et tu, Brute, the official lie, is about what you’d expect to get from them—it says exactly nothing. The moment of assassination is the moment when power and the ignorance of power come together, with Death as validator. When one speaks to the other then it is not to pass the time of day with et-tu-Brutes. What passes is a truth so terrible that history—at best a conspiracy, not always among gentlemen, to defraud—will never admit it. The truth will be repressed or in ages of particular elegance be disguised as something else. What will Rathenau, past the moment, years into a new otherside existence, have to say about the old dispensation? Probably nothing as incredible as what he might have said just as the shock flashed his mortal nerves, as the Angel swooped in…");
 
-        KasutajaAken aken = new KasutajaAken(testsone);
-        //ajutine paralleeljooksutamistest
-        System.out.println("\nmulti-track drifting\n");
+        //Aken init.
+        KasutajaAken aken = new KasutajaAken(algtekst);
 
         //Tujukas
-
-
         StringBuilder sone_ehitaja = new StringBuilder(aken.getTujutekst());
 
-
+        //Loop
         int ajaluger = 0;
         while(true){
             ajaluger++;
